754b70290cffe707491a53356c3ea98baa644e5d
tsoupionis <thomassoup@yahoo.com>
2023-02-12 23:36:01 -0500

    Dice Challenge Update 2/12

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-var randomNumber1 = Math.round(Math.random() * 6);
+var randomNumber1 = Math.round(Math.random() * 5) + 1;
+var randomNumber2 = Math.round(Math.random() * 5) + 1;
+
+var images1 = "images/dice" + randomNumber1 + ".png";
+var images2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector("img.img1").setAttribute("src", images1);
+document.querySelector("img.img2").setAttribute("src", images2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
